eb13e92644a0df5fdf35d93124ba1992b61347ed
Fernando Parra <fernandoxe@gmail.com>
2023-02-12 20:06:57 +0100

    chore: video page

---
+++ src/pages/VideoPage.tsx
@@ -0,0 +1,12 @@
+import { Video } from '../components/Video';
+
+export interface VideoPageProps {
+}
+
+export const VideoPage = () => {
+  return (
+    <div className="p-4">
+      <Video id="Anti-Hero.Taylor.Swift.1080" />
+    </div>
+  );
+};
