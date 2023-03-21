ae46954fe273364495896067f6c3e0ead1a55e8d
Fernando Parra <fernandoxe@gmail.com>
2023-03-21 21:46:36 +0100

    chore: dev proxy

---
+++ src/setupProxy.ts
@@ -0,0 +1,12 @@
+export const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// const TARGET = 'https://target'; // media host
+// module.exports = function(app: any) {
+//   app.use(
+//     '/media',
+//     createProxyMiddleware({
+//       target: TARGET,
+//       changeOrigin: true,
+//     })
+//   );
+// };
