b2b77df72a96aecf95608bc8caca8c846128a066
Fernando Parra <fernandoxe@gmail.com>
2023-04-22 23:50:54 +0200

    chore: outline

--- src/components/Controls/Reactions/index.tsx
+++ src/components/Controls/Reactions/index.tsx
@@ -12,36 +12,43 @@ export const Reactions = ({onReaction}: ReactionsProps) => {
   return(
     <div className="flex gap-4 text-lg">
       <button
+        className="outline-none"
         onClick={() => reaction(ReactionType.love)}
       >
         ❤️
       </button>
       <button
+        className="outline-none"
         onClick={() => reaction(ReactionType.hahaha)}
       >
         😂
       </button>
       <button
+        className="outline-none"
         onClick={() => reaction(ReactionType.sad)}
       >
         😢
       </button>
       <button
+        className="outline-none"
         onClick={() => reaction(ReactionType.pleading)}
       >
         🥺
       </button>
       <button
+        className="outline-none"
         onClick={() => reaction(ReactionType.angry)}
       >
         😠
       </button>
       <button
+        className="outline-none"
         onClick={() => reaction(ReactionType.cry)}
       >
         😭
       </button>
       <button
+        className="outline-none"
         onClick={() => reaction(ReactionType.thinking)}
       >
         🤔
